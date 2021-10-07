fa35cabafe9467aaaa1f5f95a9628d4e8a51ecab
Kristian Väisänen <kippeves@gmail.com>
2021-10-07 09:47:40 +0200

    Lägger till kommentarer

--- SavingsAccount.cs
+++ SavingsAccount.cs
@@ -9,16 +9,30 @@ namespace Bank
         private readonly string             AccountType = "Savings";
         private readonly int                AccountNo;
 
+        /// <summary>
+        /// Skapar ett bankkonto
+        /// </summary>
+        /// <param name="AccountNo">Ett bankkontonummer.</param>
         public SavingsAccount(int AccountNo)
         {
             this.AccountNo = AccountNo;
         }
 
+
+        /// <summary>
+        /// Sätter in pengar på ett konto
+        /// </summary>
+        /// <param name="amount">Ett hel- eller decimaltal som skall sättas in.</param>
         public void DepositAmount(decimal amount)
         {
             this.Amount += amount;
         }
 
+        /// <summary>
+        /// Tar ut pengar från ett konto.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>Om summan är mindre eller lika med mängden i kontot så returneras true, annars false.</returns>
         public bool WithdrawAmount(decimal amount)
         {
             if ((this.Amount - amount) >= 0)
@@ -29,30 +43,55 @@ namespace Bank
             else return false;
         }
 
+        /// <summary>
+        /// Hämtar räntan för kontot.
+        /// </summary>
+        /// <returns></returns>
         public double GetInterest() 
         {
             return Interest;
         }
 
+        /// <summary>
+        /// Hämtar värdet pengar som finns i kontot.
+        /// </summary>
+        /// <returns>Ett tal av typen Decimal.</returns>
         public decimal GetAmount()
         {
             return Amount;
         }
 
+        /// <summary>
+        /// Räknar ut räntevärdet för ett konto
+        /// </summary>
+        /// <returns></returns>
         public Decimal CalculateInterest()
         {
             return (this.Amount * ((decimal)Interest) / 100);
         }
 
+        /// <summary>
+        /// Hämtar kontonumret för ett konto
+        /// </summary>
+        /// <returns>Ett kontonummer i heltalsform</returns>
         public int GetAccountNo() {
             return AccountNo;
         }
 
+        /// <summary>
+        /// Vilken kontotyp som kontot har
+        /// Kontotyp är för tillfället låst på "Savings"
+        /// </summary>
+        /// <returns>En sträng med kontotyp</returns>
         public string GetAccountType()
         {
             return AccountType;
         }
 
+        /// <summary>
+        /// Visar information om kontot.
+        /// </summary>
+        /// <returns></returns>
         public string ShowAccount()
         {
             return AccountNo + "\t" + Amount + "\t" + AccountType + "\t" + CalculateInterest();
